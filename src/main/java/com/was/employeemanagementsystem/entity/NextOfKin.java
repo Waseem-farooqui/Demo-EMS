551caee94c5fb82668d1e0544871258a36d73dd1
waseem <waseem.farooqui19@gmail.com>
2025-12-03 19:17:43 +0500

    Version 1.1.1 changes and fixes.

---
+++ src/main/java/com/was/employeemanagementsystem/entity/NextOfKin.java
@@ -0,0 +1,39 @@
+package com.was.employeemanagementsystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "next_of_kin")
+public class NextOfKin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "contact")
+    private String contact;
+
+    @Column(name = "address", columnDefinition = "TEXT")
+    private String address;
+
+    @Column(name = "relationship")
+    private String relationship;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    @JsonIgnore
+    @ToString.Exclude
+    private Employee employee;
+}
