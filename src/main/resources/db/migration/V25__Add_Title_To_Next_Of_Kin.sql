ba991d5e358ee2ab23bf8af262623e7cb21eec4e
waseem <waseem.farooqui19@gmail.com>
2025-12-05 01:35:11 +0500

    SSL Support added.

--- src/main/resources/db/migration/V25__Add_Title_To_Next_Of_Kin.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Add title column to next_of_kin table
--- Description: Adds a title field (Mr, Mrs, Miss, etc.) to next of kin entries,
---              matching the same functionality as in previous employment and personal information sections.
-
-ALTER TABLE next_of_kin
-ADD COLUMN title VARCHAR(50) NULL COMMENT 'Title: Mr, Mrs, Miss, Ms, Dr, Prof, etc.';
-
